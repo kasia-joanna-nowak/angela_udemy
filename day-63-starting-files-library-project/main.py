d654d3f392f852b682b4879e7631cfe119367290
Kasia Nowak <nowak.katarzyna.joanna@gmail.com>
2024-02-26 14:08:53 +0100

    importing sqlalchemy

--- day-63-starting-files-library-project/main.py
+++ day-63-starting-files-library-project/main.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1112224444'
