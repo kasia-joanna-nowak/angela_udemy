eeab8fd3ddebe970d66aaaedbdd6e52ee968acb0
Kasia Nowak <nowak.katarzyna.joanna@gmail.com>
2024-02-28 09:25:03 +0100

    change of key names

--- day-63-starting-files-library-project/main.py
+++ day-63-starting-files-library-project/main.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 from flask_wtf import FlaskForm
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
 
 
 
@@ -23,21 +20,20 @@ class BookForm(FlaskForm):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
         if request.method == "POST":
             new_book = {
-            "book_title": request.form["book"],
-            "book_author":  request.form["author"],
-            "book_rating": request.form["rating"]}
+            "title": request.form["book"],
+            "author":  request.form["author"],
+            "rating": request.form["rating"]}
+            
             all_books.append(new_book)
-            return render_template('index.html')
+            return redirect(url_for('home'))
         return render_template('add.html')
-
-
 
 
 if __name__ == "__main__":
