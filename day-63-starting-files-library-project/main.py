deb518422bb96d6a81320d562dd779eaf6b7abe6
Kasia Nowak <nowak.katarzyna.joanna@gmail.com>
2024-02-29 09:19:43 +0100

    creating new data base

--- day-63-starting-files-library-project/main.py
+++ day-63-starting-files-library-project/main.py
@@ -6,38 +6,48 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 
-
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1112224444'
 
-all_books = []
+class Base(DeclarativeBase):
+  pass
 
 
-class BookForm(FlaskForm):
-    book = StringField('Book Name', validators=[DataRequired()])
-    author = StringField('Book Author', validators=[DataRequired()])
-    rating = StringField('Rating', validators=[DataRequired()])
-    add = SubmitField('Add Book')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True,nullable=False)
+    author: Mapped[str] = mapped_column(String(250),unique=False,nullable=False)
+    rating: Mapped[float] = mapped_column(Float,unique=False,nullable=False)
+    
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html',books=all_books)
+    result = db.session.execute(db.select(Book).order_by(Book.title)).scalars()
+
+    return render_template('index.html',books=result)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
-        if request.method == "POST":
-            new_book = {
-            "title": request.form["book"],
-            "author":  request.form["author"],
-            "rating": request.form["rating"]
-            }
+    if request.method == "POST":
+        new_book = Book(
 
-            all_books.append(new_book)
-            return redirect(url_for('home'))
-        return render_template('add.html')
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 
        
