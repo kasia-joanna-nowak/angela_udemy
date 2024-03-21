fbcdc207e6d9d374565b72a69cb5bc91f59c1cc7
Kasia Nowak <nowak.katarzyna.joanna@gmail.com>
2024-03-21 19:26:08 +0100

    created query to DB to get posts

--- day-67-starting-files-upgraded-blog/main.py
+++ day-67-starting-files-upgraded-blog/main.py
@@ -9,18 +9,6 @@ from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -52,18 +40,24 @@ with app.app_context():
 @app.route('/')
 def get_all_posts():
     # TODO: Query the database for all the posts. Convert the data to a python list.
-    posts = []
+    post_query = db.session.execute(db.select(BlogPost))
+    posts = post_query.scalars().all()
     return render_template("index.html", all_posts=posts)
 
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
 
 
 # TODO: add_new_post() to create a new blog post
+@app.route("/new-post", methods = ["GET", "POST"])
+def add_new_post():
+    if request.me
+    
+    return render_template("make-post.html")
 
 # TODO: edit_post() to change an existing blog post
 
