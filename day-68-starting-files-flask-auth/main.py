e9840c84261e1197c19a775b8710bc47dd8d2e46
Kasia Nowak <nowak.katarzyna.joanna@gmail.com>
2024-04-02 14:28:59 +0200

    added function- generete_password_hash

--- day-68-starting-files-flask-auth/main.py
+++ day-68-starting-files-flask-auth/main.py
@@ -34,10 +34,14 @@ def home():
 @app.route('/register', methods = ["POST", "GET"])
 def register():
     if request.method == "POST":
+        hash_and_salted_password = generate_password_hash(
+            request.form.get("password"),
+            method = "pbkdf2:sha256",
+            salt_length= 8)
         new_user = User(
             email=request.form.get("email"),
-            password=request.form.get("password"),
-            name = request.form.get("name")
+            name = request.form.get("name"),
+            password = hash_and_salted_password
         )
         db.session.add(new_user)
         db.session.commit()
